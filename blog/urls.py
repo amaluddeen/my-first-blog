b2fabe613625150d967f24f19d2b114e70f07490
amal <amal.sabo@aun.edu.ng>
2016-01-26 13:47:30 +0100

    My Django Girls app, first commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from .forms import PostForm
 
 urlpatterns = [
     url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+	url(r'^post/new/', views.post_detail),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
